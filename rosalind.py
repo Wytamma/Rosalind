f2d9f6987aeb63de3c66936f2f446adf8c828ab8
Wytamma Wirth <wytamma.wirth@me.com>
2020-05-19 16:09:56 +1000

    :sparkles: coloured output

--- rosalind.py
+++ rosalind.py
@@ -4,27 +4,29 @@ from pathlib import Path
 from python_village import ini3, ini4, ini5, ini6
 
 def main(
-    problem: str = typer.Argument(...), 
-    dataset: Path = typer.Option(None, "--dataset", "-d", help="Path to dataset."),
+    name_of_problem: str = typer.Argument(...), 
+    path_to_dataset: Path = typer.Option(None, "--dataset", "-d", help="Path to dataset."),
     test: bool = typer.Option(False, "--test", "-t", help="Test the soultion.")
     ):
     try:
-        problem = eval( problem )
+        problem = eval( name_of_problem )
     except NameError:
-        typer.echo(f"The problem with name: '{problem}' in not defined, you may have a typo or forgot to import it.")
+        typer.echo(typer.style(f"The problem with name: '{name_of_problem}' in not defined, you may have a typo or forgot to import it.", fg=typer.colors.WHITE, bg=typer.colors.RED))
         raise typer.Abort()
-    if not dataset:
+    if not path_to_dataset:
         output = problem.solution(problem.SAMPLE_DATASET)
         typer.echo(output)
         if test:
             try:
                 assert output == problem.SAMPLE_OUTPUT
-                typer.echo('The solution is correct!')
+                typer.echo(typer.style('\nThe solution is correct!', fg=typer.colors.GREEN, bold=True))
             except:
-                typer.echo('The soultion is not correct!\n') 
-                typer.echo(f'Correct output: {problem.SAMPLE_OUTPUT}')
+                typer.echo(typer.style('\nThe soultion is not correct!', fg=typer.colors.RED, bold=True)) 
+                typer.echo('Correct output:\n')
+                typer.echo(problem.SAMPLE_OUTPUT)
+
     else:
-        with open(dataset) as f:
+        with open(path_to_dataset) as f:
             dataset_lines = f.readlines()
         typer.echo(problem.solution(dataset_lines))
 
