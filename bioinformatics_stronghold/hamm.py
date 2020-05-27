0603f3977f53ad0359a61f14c000298f7bd96581
Wytamma Wirth <wytamma.wirth@me.com>
2020-05-27 20:57:37 +1000

    :bug: fixed type annotation

--- bioinformatics_stronghold/hamm.py
+++ bioinformatics_stronghold/hamm.py
@@ -2,7 +2,7 @@ SAMPLE_DATASET = """GAGCCTACTAACGGGAT
 CATCGTAATGACGGCCT"""
 SAMPLE_OUTPUT = 7
 
-def solution(dataset: list) -> str:
+def solution(dataset: list) -> int:
     """"there is a '\n' on the end of the first line but zip() will only go to the sortest length.
     If both lines have '\n's then they will be equivalent and won't be counted"""
-    return sum(i != j for i, j in zip(*dataset)) 
+    return sum(i != j for i, j in zip(*dataset))
