fa53165c8912d15fc2a255a0c908b4910836fc23
Wytamma Wirth <wytamma.wirth@me.com>
2020-05-27 20:39:42 +1000

    :sparkles: Solved problem: revc

--- bioinformatics_stronghold/dna.py
+++ bioinformatics_stronghold/dna.py
@@ -5,4 +5,4 @@ SAMPLE_OUTPUT = """20 12 17 21"""
 
 def solution(dataset: list) -> str:
     c = Counter(dataset[0].strip()) # string is on the first line
-    return " ".join([str(c[nt]) for nt in sorted(c)])
+    return " ".join(str(c[nt]) for nt in sorted(c))
---
+++ bioinformatics_stronghold/revc.py
@@ -0,0 +1,6 @@
+SAMPLE_DATASET = """AAAACCCGGT"""
+SAMPLE_OUTPUT = """ACCGGGTTTT"""
+
+def solution(dataset: list) -> str:
+    complements = {"A":"T", "T":"A", "G":"C", "C":"G"}
+    return "".join(complements[nt] for nt in reversed(dataset[0].strip()))
