2c7e57d6815e48c531392d664c1615e49faec99e
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-03 15:27:49 +1000

    :sparkles: solved problem: fib

---
+++ bioinformatics_stronghold/fib.py
@@ -0,0 +1,18 @@
+SAMPLE_DATASET = """5 3"""
+SAMPLE_OUTPUT = """19"""
+
+
+def population_size(n: int, k: int) -> int:
+    """
+    Returns the population at time 'n' given 'k' genration size.
+    Fn = Fn−1 + (Fn−2 * k)
+    """
+    if n <= 2:
+        # F1 = F2 = 1
+        return 1
+    return population_size(n - 1, k) + (population_size(n - 2, k) * k)
+
+
+def solution(dataset: list) -> str:
+    n, k = map(int, dataset[0].strip().split())
+    return str(population_size(n, k))
--- rosalind.py
+++ rosalind.py
@@ -2,7 +2,7 @@ import typer
 from pathlib import Path
 
 from python_village import ini3, ini4, ini5, ini6
-from bioinformatics_stronghold import dna, rna, revc, hamm
+from bioinformatics_stronghold import dna, rna, revc, hamm, fib
 
 
 def main(
