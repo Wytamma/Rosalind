5bf4c564827e19cb855fa37342f0b3675d3ea08e
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-07 13:54:45 +1000

    :recycle: Moved general code to Bio module

--- bioinformatics_stronghold/gc.py
+++ bioinformatics_stronghold/gc.py
@@ -1,6 +1,4 @@
-from typing import List
-from dataclasses import dataclass
-
+from .Bio import read_fasta
 
 SAMPLE_DATASET = """>Rosalind_6404
 CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
@@ -13,37 +11,6 @@ CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
 TGGGAACCTGCGGGCAGTAGGTGGAAT"""
 SAMPLE_OUTPUT = """Rosalind_0808
 60.919540"""
-
-
-@dataclass
-class Seq:
-    """Class for nucleotide sequences"""
-
-    sequence: str
-    id: str
-
-    def __len__(self) -> int:
-        return len(self.sequence)
-
-    @property
-    def gc(self) -> float:
-        """Return the GC content of the sequence"""
-        g = self.sequence.count("G")
-        c = self.sequence.count("C")
-        return (g + c) / len(self) * 100
-
-
-def read_fasta(lines_of_fasta_file: list) -> List[Seq]:
-    """Turn the lines of a fasta file into a list of Seq objects"""
-    sequences = []
-    full_seq = ""
-    for line in reversed(lines_of_fasta_file):
-        if line.startswith(">"):
-            sequences.append(Seq(sequence=full_seq, id=line.strip()[1:]))
-            full_seq = ""
-            continue
-        full_seq += line.strip()
-    return sequences
 
 
 def solution(dataset: list) -> str:
