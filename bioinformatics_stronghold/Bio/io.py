fc9895e15f7e0538772e2f60886b670447e9b1be
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-07 14:16:23 +1000

    :recycle: made IO module

---
+++ bioinformatics_stronghold/Bio/io.py
@@ -0,0 +1,14 @@
+from typing import List
+from .seq import Seq
+
+def read_fasta(lines_of_fasta_file: list) -> List[Seq]:
+    """Turn the lines of a fasta file into a list of Seq objects"""
+    sequences = []
+    full_seq = ""
+    for line in reversed(lines_of_fasta_file):
+        if line.startswith(">"):
+            sequences.append(Seq(sequence=full_seq, id=line.strip()[1:]))
+            full_seq = ""
+            continue
+        full_seq += line.strip()
+    return sequences
