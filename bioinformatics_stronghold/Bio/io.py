c51d7f9556f90652bfc48bdfe8b8a0316146dd6c
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-11 22:58:33 +1000

    :bug: Fixed bug in read_fasta

--- bioinformatics_stronghold/Bio/io.py
+++ bioinformatics_stronghold/Bio/io.py
@@ -5,11 +5,11 @@ from .seq import Seq
 def read_fasta(lines_of_fasta_file: list) -> List[Seq]:
     """Turn the lines of a fasta file into a list of Seq objects"""
     sequences = []
-    full_seq = ""
+    full_seq = []
     for line in reversed(lines_of_fasta_file):
         if line.startswith(">"):
-            sequences.append(Seq(sequence=full_seq, id=line.strip()[1:]))
-            full_seq = ""
+            sequences.append(Seq(sequence="".join(full_seq[::-1]), id=line.strip()[1:]))
+            full_seq = []
             continue
-        full_seq += line.strip()
-    return sequences
+        full_seq.append(line.strip())
+    return sequences[::-1]  # return order
