00ac984de90f86c7cea267ababe6bc7267931575
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-11 16:22:00 +1000

    :art: Methods return new seq objects

--- bioinformatics_stronghold/Bio/seq.py
+++ bioinformatics_stronghold/Bio/seq.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Generator
 from collections import Counter
+from __future__ import annotations
 
 codon_table = """UUU F      CUU L      AUU I      GUU V
 UUC F      CUC L      AUC I      GUC V
@@ -27,11 +28,27 @@ class Seq:
     """Class for nucleotide sequences"""
 
     sequence: str
-    id: str
+    id: str = None
     codons: dict = codons
+
+    def __repr__(self):
+        return f"Seq({self.Seq[:10]}..., id='{self.id[:15] if self.id}')"
+    
+    def __str__(self):
+        return self.sequence
 
     def __len__(self) -> int:
         return len(self.sequence)
+
+    def __neg__(self) -> Seq:
+        """Negating a Seq object (i.e. !Seq) will return the reverse complement of that sequence"""
+        return self.reverse_complement()
+
+    def __add__(self, sequence: Seq) -> Seq:
+        """Adding two sequence objects (i.e. Seq1 + Seq2) returns a new Seq object that is the 
+        concatenation of the two objects sequences. ID is taken from eh first object""" 
+        new_sequence = self.sequence + sequence.sequence
+        return Seq(new_sequence, self.id)
 
     @property
     def gc(self) -> float:
@@ -49,16 +66,23 @@ class Seq:
         """Return a generator for kmers of length n"""
         return (self.sequence[i : i + n] for i in range(0, len(self.sequence), step))
 
-    def transcribe(self) -> Seq:
-        return Seq(self.sequence.replace("T", "U"), id-=self.id)
+    def reverse_complement(self) -> Seq:
+        complements = {"A": "T", "T": "A", "G": "C", "C": "G"}
+        revc = "".join(complements[nt] for nt in reversed(self.sequence))
+        return Seq(revc, self.id)
 
-    def translate(self) -> str:
+    def transcribe(self) -> Seq:
+        return Seq(self.sequence.replace("T", "U"), self.id)
+
+    def translate(self) -> Seq:
         """
         Return the translated sequence.
         *Currently stop signals are ignored.*
         """
-        return "".join(
+        AA = "".join(
             self.codons[self.sequence[i : i + 3]]
             for i in range(0, len(self.sequence), 3)
             if self.codons[self.sequence[i : i + 3]] != "Stop"
         )
+        return Seq(AA, self.id)
+
