1899e97f758f12ffefd0671fcd2dd5cb3a2fe8bf
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-15 23:02:43 +1000

    :zap: O(n * ( n - 1 ) / 2) == O(n**2)

--- bioinformatics_stronghold/grph.py
+++ bioinformatics_stronghold/grph.py
@@ -15,11 +15,11 @@ Rosalind_0498 Rosalind_0442
 Rosalind_2391 Rosalind_2323"""
 
 
-def solution(dataset: list) -> str:
+def slow_solution(dataset: list) -> str:
     sequences = read_fasta(lines=dataset)
     k = 3
     graph = []
-    for s in sequences:  # n^2
+    for s in sequences:  # O(n**2)
         for t in sequences:
             if s == t:
                 # stop cycling
@@ -29,5 +29,25 @@ def solution(dataset: list) -> str:
     return "\n".join([f"{edge[0]} {edge[1]}" for edge in graph])
 
 
+def solution(dataset: list) -> str:
+    sequences = read_fasta(lines=dataset)
+    k = 3
+    edges = []
+    nodes = []
+    for s in sequences:  # O(n * ( n - 1 ) / 2) == O(n**2), but runs twice as fast
+        # check what other nodes in the graph s connects to
+        for t in nodes:
+            if s.sequence.endswith(t[:k]):
+                edges.append((s.id, t.id))
+            if t.sequence.endswith(s[:k]):
+                edges.append((t.id, s.id))
+        # add node to graph
+        nodes.append(s)
+    return "\n".join([f"{edge[0]} {edge[1]}" for edge in edges])
+
+
 def test_solution():
-    assert solution(SAMPLE_DATASET.splitlines(True)) == SAMPLE_OUTPUT
+    assert slow_solution(SAMPLE_DATASET.splitlines(True)) == SAMPLE_OUTPUT
+    assert set(solution(SAMPLE_DATASET.splitlines(True)).splitlines()) == set(
+        SAMPLE_OUTPUT.splitlines()
+    )
