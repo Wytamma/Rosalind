6aa21c24a58c1e9e9781b795d0f32248d27749fb
Wytamma Wirth <wytamma.wirth@me.com>
2021-02-23 11:46:53 +1000

    :art: factoring solutions import

--- bioinformatics_stronghold/__init__.py
+++ bioinformatics_stronghold/__init__.py
@@ -1,8 +1,10 @@
 import os
 from importlib import import_module
 
+package = "bioinformatics_stronghold"
+
 solutions = {
-    f[:-3]: import_module(f".{f[:-3]}", "bioinformatics_stronghold")
-    for f in os.listdir("bioinformatics_stronghold")
+    f[:-3]: import_module(f".{f[:-3]}", package)
+    for f in os.listdir(package)
     if f.endswith(".py")
 }
