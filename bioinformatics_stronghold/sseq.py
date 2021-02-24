fbb56e725e08e8e9c880b355bdfd21e18fe8e322
Wytamma Wirth <wytamma.wirth@me.com>
2021-02-25 08:43:45 +1000

    :art: formating black

--- bioinformatics_stronghold/sseq.py
+++ bioinformatics_stronghold/sseq.py
@@ -6,26 +6,28 @@ ACGTACGTGACG
 GTA"""
 SAMPLE_OUTPUT = """3 4 5"""  # changed for multiple soultions
 
+
 def solution(dataset: list) -> str:
     sequences = read_fasta(lines=dataset)
     DNA = sequences[0]
     subsequence = sequences[1]
+
     indices = []
     offest = 0
-
     for base in subsequence:
         loc = DNA[offest:].find(base)
         offest = loc + offest + 1
         indices.append(str(offest))
-    
+
     # sanity check
     for i, idx in enumerate(indices):
         try:
-            assert subsequence[i] == DNA[int(idx)-1]
+            assert subsequence[i] == DNA[int(idx) - 1]
         except:
-            print (f"{subsequence[i]} != {DNA[int(idx)-1]} at {idx}")
+            print(f"{subsequence[i]} != {DNA[int(idx)-1]} at {idx}")
 
     return " ".join(indices)
+
 
 def test_solution():
     assert solution(SAMPLE_DATASET.splitlines(True)) == SAMPLE_OUTPUT
