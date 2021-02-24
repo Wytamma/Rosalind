7c7ab274634d274c26ef85268d1a8c4306a4b4cf
Wytamma Wirth <wytamma.wirth@me.com>
2021-02-24 11:23:36 +1000

    :art: black

--- bioinformatics_stronghold/splc.py
+++ bioinformatics_stronghold/splc.py
@@ -8,12 +8,14 @@ ATCGGTCGAA
 ATCGGTCGAGCGTGT"""
 SAMPLE_OUTPUT = """MVYIADKQHVASREAYGHMFKVCA"""
 
+
 def solution(dataset: list) -> str:
     sequences = read_fasta(lines=dataset)
-    DNA, *introns  = sequences
+    DNA, *introns = sequences
     for intron in introns:
-        DNA = DNA.substitute(intron.sequence, '')
+        DNA = DNA.substitute(intron.sequence, "")
     return DNA.transcribe().translate()
+
 
 def test_solution():
     assert solution(SAMPLE_DATASET.splitlines(True)) == SAMPLE_OUTPUT
