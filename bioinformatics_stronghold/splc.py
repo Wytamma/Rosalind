79f4c1e336b0fa53fb8bd14b3b5dbed94c1b2230
Wytamma Wirth <wytamma.wirth@me.com>
2021-02-24 11:19:54 +1000

    :sparkles: solved splc

---
+++ bioinformatics_stronghold/splc.py
@@ -0,0 +1,19 @@
+from pyoinformatics import read_fasta
+
+SAMPLE_DATASET = """>Rosalind_10
+ATGGTCTACATAGCTGACAAACAGCACGTAGCAATCGGTCGAATCTCGAGAGGCATATGGTCACATGATCGGTCGAGCGTGTTTCAAAGTTTGCGCCTAG
+>Rosalind_12
+ATCGGTCGAA
+>Rosalind_15
+ATCGGTCGAGCGTGT"""
+SAMPLE_OUTPUT = """MVYIADKQHVASREAYGHMFKVCA"""
+
+def solution(dataset: list) -> str:
+    sequences = read_fasta(lines=dataset)
+    DNA, *introns  = sequences
+    for intron in introns:
+        DNA = DNA.substitute(intron.sequence, '')
+    return DNA.transcribe().translate()
+
+def test_solution():
+    assert solution(SAMPLE_DATASET.splitlines(True)) == SAMPLE_OUTPUT
